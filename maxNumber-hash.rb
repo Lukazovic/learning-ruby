5b8db7294ff7566ea9b688e6aa7a65a2fa7c96a8
Lukazovic <lucas10.vieira.lv@gmail.com>
2020-06-30 11:12:29 -0300

    Add a fucntion to search the max value in a hash

---
+++ maxNumber-hash.rb
@@ -0,0 +1,9 @@
+numbers = {A: 10, B: 302, C: 20, D: 25, E: 15}
+maxNumber = {test: 0}
+
+numbers.map do |key, value|
+  maxValue = maxNumber.values[0]
+  maxNumber = {"#{key}": value} if maxValue && value > maxValue
+end
+
+puts maxNumber
