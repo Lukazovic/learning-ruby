9d7292f18bea527e105569ef1cd1fe01d9db64f6
Lukazovic <lucas10.vieira.lv@gmail.com>
2020-06-30 12:31:16 -0300

    Add validate CPF

---
+++ cpf_cnpj/index.rb
@@ -0,0 +1,17 @@
+require "cpf_cnpj"
+
+def checkCPF(cpf)
+  valid = false
+  valid = CPF.valid?(cpf) if CPF.valid?(cpf)
+
+  if valid === true
+    puts "O CPF é válido!"
+  else
+    puts "O CPF não é válido!"
+  end
+end
+
+print "Informe um CPF: "
+cpf = gets.chomp.to_s
+
+checkCPF(cpf)
